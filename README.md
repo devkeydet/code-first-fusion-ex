c963b92c760b5c23729c9ad8ded3832fa7a57c7b
devkeydet <devkeydet@gmail.com>
2021-12-22 10:09:51 -0500

    wip

--- README.md
+++ README.md
@@ -109,3 +109,36 @@ Export/Unpack metadata from changes made in make.powerapps.com
 ```
 . .\export-unpack.ps1
 ```
+# ADD CANVAS APP WITH PCF COMPONENT
+
+Create new canvas app in solution using make.powerapps.com and add PCF component (save and publish)
+
+update **export-unpack.ps1** to look like the following
+```
+function Export-Unpack ([Parameter(Mandatory)]$solutionName) {
+    pac solution publish
+    $tempDirectory = "temp"
+    $unmanaged = "$tempDirectory/$solutionName.zip"
+    $managed = "$tempDirectory/$solutionName" + "_managed.zip"
+    pac solution export --path $unmanaged --name $solutionName --managed false
+    pac solution export --path $managed --name $solutionName --managed true
+    $unpackedSolutionDirectory = "src/solutions/$solutionName/unpacked-solution"
+    pac solution unpack -f $unpackedSolutionDirectory -z $unmanaged -p both --allowDelete true
+    $unpackedCanvasAppsDirectory = "$unpackedSolutionDirectory/CanvasApps"
+    $hasCanvasApps = Test-Path $unpackedCanvasAppsDirectory
+    if ($hasCanvasApps) {
+        $msappFiles = Get-ChildItem $unpackedCanvasAppsDirectory -Filter *.msapp
+        foreach ($msapp in $msappFiles) { 
+            pac canvas unpack --msapp $msapp.FullName --sources ($msapp.FullName.Replace('.msapp', "_msapp")) 
+        }
+    }
+    Remove-Item $tempDirectory -Recurse
+}
+
+Export-Unpack
+```
+
+Export/Unpack metadata from changes made in make.powerapps.com
+```
+. .\export-unpack.ps1
+```
--- export-unpack.ps1
+++ export-unpack.ps1
@@ -1,8 +1,21 @@
-pac solution publish
-$tempDirectory = "temp"
-$unmanaged = "$tempDirectory/$solutionName.zip"
-$managed = "$tempDirectory/$solutionName" + "_managed.zip"
-pac solution export --path $unmanaged --name $solutionName --managed false
-pac solution export --path $managed --name $solutionName --managed true
-pac solution unpack -f src/solutions/$solutionName/unpacked-solution -z $unmanaged -p both --allowDelete true    
-Remove-Item $tempDirectory -Recurse
+function Export-Unpack ([Parameter(Mandatory)]$solutionName) {
+    pac solution publish
+    $tempDirectory = "temp"
+    $unmanaged = "$tempDirectory/$solutionName.zip"
+    $managed = "$tempDirectory/$solutionName" + "_managed.zip"
+    pac solution export --path $unmanaged --name $solutionName --managed false
+    pac solution export --path $managed --name $solutionName --managed true
+    $unpackedSolutionDirectory = "src/solutions/$solutionName/unpacked-solution"
+    pac solution unpack -f $unpackedSolutionDirectory -z $unmanaged -p both --allowDelete true
+    $unpackedCanvasAppsDirectory = "$unpackedSolutionDirectory/CanvasApps"
+    $hasCanvasApps = Test-Path $unpackedCanvasAppsDirectory
+    if ($hasCanvasApps) {
+        $msappFiles = Get-ChildItem $unpackedCanvasAppsDirectory -Filter *.msapp
+        foreach ($msapp in $msappFiles) { 
+            pac canvas unpack --msapp $msapp.FullName --sources ($msapp.FullName.Replace('.msapp', "_msapp")) 
+        }
+    }
+    Remove-Item $tempDirectory -Recurse
+}
+
+Export-Unpack
